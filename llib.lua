6ca10a833299bf36db600971ab6000b90b38eade
Lixquid <Lixquid@users.noreply.github.com>
2014-11-28 16:13:31 +0000

    Added Boilerplate and Namespace

---
+++ llib.lua
@@ -0,0 +1,13 @@
+--[[----------------------------------------------------------------------------
+    LLib - v0.1.0
+
+    A lightweight library of Lua functions
+    Made with <3 by Lixquid
+--]]----------------------------------------------------------------------------
+
+llib = {}
+llib.version = "0.1.0"
+llib.invasive = true
+
+local ns = llib.invasive and _G or llib
+
