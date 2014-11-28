dfcea9dc68c5b0d2053de9063ebbe0a343c3d035
Lixquid <Lixquid@users.noreply.github.com>
2014-11-28 16:16:04 +0000

    Added assert functions

--- llib.lua
+++ llib.lua
@@ -11,3 +11,19 @@ llib.invasive = true
 
 local ns = llib.invasive and _G or llib
 
+--[[----------------------------------------------------------------------------
+    Assert
+--]]----------------------------------------------------------------------------
+
+function ns.assertNumber( object )
+  return tonumber( object )
+end
+
+function ns.assertString( object )
+  return object ~= nil and tostring( object ) or nil
+end
+
+function ns.assertTable( object )
+  return type( object ) == "table" and object or nil
+end
+
