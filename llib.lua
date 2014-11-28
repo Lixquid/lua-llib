81aa0ec741e97394e2fef7ffc97296d04c084490
Lixquid <Lixquid@users.noreply.github.com>
2014-11-28 16:22:25 +0000

    Added Math functions

--- llib.lua
+++ llib.lua
@@ -45,3 +45,76 @@ function ns.toBool( object )
   return true
 end
 
+--[[----------------------------------------------------------------------------
+    Math
+--]]----------------------------------------------------------------------------
+ns.math = ns.math or {}
+local nsl = ns.math
+
+function nsl.clamp( number, minimum, maximum )
+  return math.max( math.min( number, maximum ), minimum )
+end
+
+function nsl.comma( number, delimiter, spacing )
+  local number, delimiter, spacing, hit =
+    tostring( number ),
+    tostring( delimiter or "," ),
+    tonumber( spacing or 3 )
+
+  while true do
+    number, hit = number:gsub(
+      "^(-?%d+)(" .. ("%d"):rep( spacing ) .. ")",
+      "%1" .. delimiter .. "%2" )
+    if hit == 0 then return number end
+  end
+end
+
+function nsl.lerp( number, target, amount )
+  return number + ( target - number ) * math.min( math.max( amount, 0 ), 1 )
+end
+
+function nsl.logB( number, base )
+  return math.log( number ) / math.log( base )
+end
+
+local metric_table = { { "k", "kilo" }, { "M", "mega" }, { "G", "giga" },
+	{ "T", "tera" }, { "P", "peta" }, { "E", "exa" }, { "Z", "zetta" },
+	{ "Y", "yotta" }, [0] = { "", "" }, [-1] = { "m", "milli" },
+	[-2] = { "u", "micro" }, [-3] = { "n", "nano" }, [-4] = { "p", "pico" },
+	[-5] = { "f", "femto" }, [-6] = { "a", "atto" }, [-7] = { "z", "zepto" },
+	[-8] = { "y", "yocto" } }
+function nsl.metric( number, base )
+  local base = base or 1000
+
+  local div = math.max( math.min( math.floor(
+    math.log( number ) / math.log( base ) ), 8 ), -8 )
+  return number / base ^ div, unpack( metric_table[ div ] )
+end
+
+function nsl.ordinal( number )
+  local last = tostring( math.floor( tonumber( number ) + 0.5 ) ):sub( -1 )
+
+  if tonumber( number ) > 10 and
+    tostring( math.floor( number + 0.5 ) ):sub( -2, -2 ) == "1" then
+
+      return "th"
+  elseif last == "1" then
+    return "st"
+  elseif last == "2" then
+    return "nd"
+  elseif last == "3" then
+    return "rd"
+  end
+  return "th"
+end
+
+function nsl.randomSign()
+  return math.random() > 0.5 and 1 or -1
+end
+
+function nsl.round( number, dp )
+  return dp
+    and math.floor( number * 10 ^ dp + 0.5 ) / 10 ^ dp
+    or math.floor( number + 0.5 )
+end
+
